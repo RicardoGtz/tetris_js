0c7f8bedb50fda1d86ed2ffcb01698a6e5fab5ab
RicardoGtz <cvricky14608@gmail.com>
2020-05-17 21:17:44 -0500

    Display to show the next tetromino added

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //The Tetrominoes
   const L_TETROMINO = [
-    [1, WIDTH + 1, WIDTH * 2 + 1, 2],
+    [0, WIDTH, WIDTH * 2, 1],
     [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH * 2 + 2],
-    [1, WIDTH + 1, WIDTH * 2 + 1, WIDTH * 2],
+    [1, WIDTH+1, WIDTH * 2+1, WIDTH * 2],
     [WIDTH, WIDTH * 2, WIDTH * 2 + 1, WIDTH * 2 + 2],
   ];
 
@@ -69,6 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 
   let squares = Array.from(document.querySelectorAll(".grid div"));
+  let nextTetrominoGrid=Array.from(document.querySelectorAll(".nextTetrinomio div"))
 
   let currentPosition = 4;
   let currentRotation = 0;
@@ -76,21 +77,12 @@ document.addEventListener("DOMContentLoaded", () => {
   //Randomly select a Tetromino
   let random = 0;
   let current = [];
+  let currentID=0;
+  let nextTetromino=[];
+  let nextID = 0;
+  createNextTetromino();
   startNewTetromino();
-  
- 
 
-  document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 87) {
-      rotateTeromino();
-    } else if (event.keyCode === 65) {
-      moveTetromino(LEFT_MOVE);
-    } else if (event.keyCode === 68) {
-      moveTetromino(RIGHT_MOVE);
-    } else if (event.keyCode === 83) {
-      moveTetrominoDown();
-    }
-  });
 
   function moveTetromino(movement) {
     undrawTetromino();
@@ -120,20 +112,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function drawTetromino() {
     current.forEach((index) => {
-      squares[currentPosition + index].classList.add("tetromino",COLOR_LIST[random]);
+      squares[currentPosition + index].classList.add("tetromino",COLOR_LIST[currentID]);
     });
   }
 
-  function undrawTetromino() {
-    current.forEach((index) => {
-      squares[currentPosition + index].classList.remove("tetromino",COLOR_LIST[random]);
+  function drawNextTetromino() {    
+    clearNextDipsplay();
+    nextTetromino.forEach((index) => {        
+      nextTetrominoGrid[index>=WIDTH?index-((WIDTH-3)*(Math.floor(index/WIDTH))):index].classList.add("tetromino",COLOR_LIST[nextID]);
     });
+  }
+
+  function undrawTetromino(){
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("tetromino",COLOR_LIST[currentID]);
+    });
+  }
+
+  function clearNextDipsplay(){           
+    nextTetrominoGrid.forEach(index => index.classList.remove("tetromino",COLOR_LIST[nextID]));    
   }
 
   function rotateTeromino() {
     undrawTetromino();
-    currentRotation = (currentRotation + 1) % 4;
-    current = THE_TETROMINOES[random][currentRotation];
+    currentRotation = (currentRotation + 1) % THE_TETROMINOES[currentID].length;
+    current = THE_TETROMINOES[currentID][currentRotation];
     drawTetromino();
   }
 
@@ -141,10 +144,20 @@ document.addEventListener("DOMContentLoaded", () => {
   timerId = setInterval(moveTetrominoDown, SPEED);
 
   function startNewTetromino() {
-    currentPosition = 4;
-    random = Math.floor(Math.random()*1000) % THE_TETROMINOES.length;
-    current = THE_TETROMINOES[random][currentRotation];
+    currentPosition = 4;  
+    currentID=nextID;  
+    current = nextTetromino;
+    createNextTetromino();
     drawTetromino();
+  }
+
+  function createNextTetromino(){
+    clearNextDipsplay();
+    currentRotation=0
+    nextID = Math.floor(Math.random()*1000) % THE_TETROMINOES.length;
+    nextTetromino=THE_TETROMINOES[nextID][currentRotation];
+    //Draw Next
+    drawNextTetromino();
   }
 
   //freeze function
@@ -160,4 +173,20 @@ document.addEventListener("DOMContentLoaded", () => {
       startNewTetromino();
     }
   }
+
+  //Controlls
+document.addEventListener("keydown", (event) => {
+    if (event.keyCode === 87) {
+      rotateTeromino();
+    } else if (event.keyCode === 65) {
+      moveTetromino(LEFT_MOVE);
+    } else if (event.keyCode === 68) {
+      moveTetromino(RIGHT_MOVE);
+    } else if (event.keyCode === 83) {
+      moveTetrominoDown();
+    }
+  });
+
 });
+
+
